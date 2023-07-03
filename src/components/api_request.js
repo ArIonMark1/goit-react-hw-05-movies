ad3bfc2fc54b3eb1f3dd234cf0cc5b6feadf25c4
ArIonMark1 <andreydatsenko777@gmail.com>
2023-07-03 11:14:43 +0200

    bugfix

--- src/components/api_request.js
+++ src/components/api_request.js
@@ -5,78 +5,80 @@ const url = 'https://api.themoviedb.org/3/';
 
 // ****************************************************************
 function returnCustomError(reject) {
-  new Promise.reject(
-    new Error(`${reject} Wrong request!! Nothing was found on your request. `)
+  const errMessage = Promise.reject(
+    new Error(`Wrong request!! ${reject.message}, nothing was found. `)
   );
+  return errMessage;
 }
 // #################################################################
 export const handleRequestPopular = async () => {
-  const request = await axios.get(`${url}trending/all/day`, {
-    params: { api_key: API_KEY, page: 1 },
-  });
-  if (request.status === 200) {
+  try {
+    const request = await axios.get(`${url}trending/all/day`, {
+      params: { api_key: API_KEY, page: 1 },
+    });
     return request.data;
+  } catch (error) {
+    return returnCustomError(error);
   }
-  returnCustomError(request);
 };
 
 // #################################################################
 export const handleRequestById = async id => {
   // Get the top level details of a movie by ID.
-  const request = await axios.get(`${url}movie/${id}`, {
-    params: {
-      api_key: API_KEY,
-    },
-  });
-  // // ********************************
-  // console.log('movieId from  component => : ', id);
-  // console.log('movieId =>: ', request);
-  // // ********************************
-  if (request.status === 200) {
+  try {
+    const request = await axios.get(`${url}movie/${id}`, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
     return request.data;
+  } catch (error) {
+    return returnCustomError(error);
   }
-  returnCustomError(request);
+  // // ********************************
 };
 
 // #################################################################
 export const handleMovieSearchList = async fTitle => {
   // Search for movies by their original, translated and alternative titles.
-  const BASE_URL = `https://api.themoviedb.org/3/search/movie`;
+  const BASE_URL = `${url}search/movie`;
 
   const params = {
     api_key: API_KEY,
     query: `${fTitle}`,
     page: 1,
   };
-
-  const request = await axios.get(BASE_URL, { params });
-  // ********************************
-  if (request.status === 200) {
+  try {
+    const request = await axios.get(BASE_URL, { params });
     return request.data;
+  } catch (error) {
+    return returnCustomError(error);
   }
-  returnCustomError(request);
+  // ********************************
 };
 
 // #################################################################
 export const handleMovieReview = async movieId => {
   // https://api.themoviedb.org/3/movie/{movie_id}/reviews
-  const request = await axios.get(`${url}movie/${movieId}/reviews`, {
-    params: { api_key: API_KEY },
-  });
-  if (request.status === 200) {
+  try {
+    const request = await axios.get(`${url}movie/${movieId}/reviews`, {
+      params: { api_key: API_KEY },
+    });
     return request.data;
+  } catch (error) {
+    return returnCustomError(error);
   }
-  returnCustomError(request);
 };
 
 // #################################################################
 export const handleMovieCredits = async movieId => {
   // https://api.themoviedb.org/3/movie/{movie_id}/credits
-  const request = await axios.get(`${url}movie/${movieId}/credits`, {
-    params: { api_key: API_KEY },
-  });
-  if (request.status === 200) {
+  try {
+    const request = await axios.get(`${url}movie/${movieId}/credits`, {
+      params: { api_key: API_KEY },
+    });
     return request.data;
+  } catch (error) {
+    return returnCustomError(error);
   }
-  returnCustomError(request);
 };
